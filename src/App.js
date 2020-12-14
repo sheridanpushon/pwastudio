d41bb46772f91dd00abf706d63a8d4cdcdbd68b1
Sheridan Pickett <sheridan@Sheridans-Air.lan>
2020-12-14 17:10:11 +0000

    added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BranchFinder from './components/BranchFinder';
 import MoreInfo from './components/MoreInfo';
 import Header from './components/Header';
+import Footer from './components/Footer'
 
 const App = () => {
   return (
